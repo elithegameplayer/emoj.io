91c8e16dcd53cc076bec514bb7e6701de09b9a8b
Mac <mac@Macs-MacBook-Pro.local>
2021-05-31 18:34:29 -0700

    pause and hint

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ let bg = "green"
 let difficulty = 1
 const one = 1
 let pause = false
+let hintRange = null;
+let hints = 3;
 
 function setCurrentEmoji() {
   currentEmoji = { emoji: random(emojis[difficulty]), pos: {} }
@@ -24,6 +26,8 @@ function setup() {
   setTargetSpot()
   setCurrentEmoji()
   frameRate(30)
+  fill('white')
+  textAlign(CENTER, CENTER)
   setTimeout(function () {
     bg = 51
   }, 250)
@@ -41,12 +45,35 @@ function wrong() {
   }, 250)
 }
 
+function hint() {
+  if (!!hints) {
+    const left = spreadX(Math.round(targetSpot.x - count / 4 < 1 ? 1 : targetSpot.x - count / 4));
+    const right = spreadX(Math.round(targetSpot.x + count / 4 > count ? count : targetSpot.x + count / 4));
+    const top = spreadY(Math.round(targetSpot.y - count / 4 < 1 ? 1 : targetSpot.y - count / 4));
+    const bottom = spreadY(Math.round(targetSpot.y + count / 4 > count ? count : targetSpot.y + count / 4));
+
+    hintRange = { left, right, top, bottom };
+    hints--
+  }
+}
+
+function spreadX(val) {
+  return map(val, 1, count, 50, width - 50)
+}
+
+function spreadY(val) {
+  return map(val, 1, count, 50, height - 50)
+}
+
 function draw() {
+  if (lives < 1) {
+    restart()
+  }
+
   if (!pause) {
     background(bg)
     textSize(size)
-    fill('white')
-    textAlign(CENTER, CENTER)
+
 
     size = 1 / count * 4 * 175
 
@@ -55,45 +82,48 @@ function draw() {
         const pos = { x: map(x, 1, count, 50, width - 50), y: map(y, 1, count, 50, height - 50) }
         if (y === targetSpot.y && x === targetSpot.x) {
           currentEmoji.pos = { ...pos }
-          text(currentEmoji.emoji.target, pos.x, pos.y)
+          if (!!hintRange) {
+            if (pos.x >= hintRange.left && pos.x <= hintRange.right && pos.y >= hintRange.top && pos.y <= hintRange.bottom) text(currentEmoji.emoji.target, pos.x, pos.y)
+          } else {
+            text(currentEmoji.emoji.target, pos.x, pos.y)
+          }
         } else {
-          text(currentEmoji.emoji.decoy, pos.x, pos.y)
+          if (!!hintRange) {
+            if (pos.x >= hintRange.left && pos.x <= hintRange.right && pos.y >= hintRange.top && pos.y <= hintRange.bottom) text(currentEmoji.emoji.decoy, pos.x, pos.y)
+          } else {
+            text(currentEmoji.emoji.decoy, pos.x, pos.y)
+          }
         }
       }
     }
     textSize(20)
     text("Level: " + level, windowWidth - 50, windowHeight - 15)
     text("Lives: " + lives, 50, windowHeight - 15)
-    text(Math.round(frameCount / 30), windowWidth - 300, windowHeight - 15);
-
-    if (lives < 1) {
-      restart()
-    }
+    text("Hints: " + hints, windowWidth * 0.4, windowHeight - 15)
+    text(Math.round(frameCount / 30), windowWidth * 0.6, windowHeight - 15);
   }
 }
 
 function mousePressed(e) {
-  if (Math.abs(mouseX - currentEmoji.pos.x) > size / 2) {
-    wrong()
-    return
-  }
-
-  if (Math.abs(mouseY - currentEmoji.pos.y) > size / 2) {
+  // they clicked the wrong one
+  if (Math.abs(mouseX - currentEmoji.pos.x) > size / 2 || Math.abs(mouseY - currentEmoji.pos.y) > size / 2) {
     wrong()
     return
   }
   // they clicked the right one
   count += inc
   level++
-  if (level % 5 == 0) {
-    difficulty += one
-  }
+
+  if (level % 5 == 0) difficulty = difficulty > 4 ? 4 : difficulty + one
+
   setCurrentEmoji()
   setTargetSpot()
+  hintRange = null;
 }
 
-function keyPressed(key) {
-  if (key == " ") {
-    pause = !pause
-  }
+function keyPressed() {
+  if (keyCode == 72) hint()
+  if (keyCode == 32) pause = !pause
+  if (pause) noLoop()
+  else loop()
 }
